41a3c8917e9767aa05c521aa6479bd9df8db1353
Asbjørn Knudsen <eaaatkn@students.eaaa.dk>
2024-03-20 12:00:34 +0100

    intial commit

---
+++ CustomerService/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿using CustomerService.Models;
+
+namespace CustomerService.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        IEnumerable<Customer> GetCustomers();
+        Customer Add(Customer newItem);
+        Customer GetById(int id);
+    //    void Remove(int id);
+    }
+}
